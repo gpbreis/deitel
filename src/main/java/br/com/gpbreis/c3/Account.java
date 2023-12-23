552be632c35708a4914bd210b125dc91553a36a1
Gustavo P. Breis <gpbreis@gmail.com>
2023-12-23 14:14:35 -0300

    iniciando commits do `capítulo 3`

---
+++ src/main/java/br/com/gpbreis/c3/Account.java
@@ -0,0 +1,15 @@
+package br.com.gpbreis.c3;
+
+public class Account {
+    private String name;
+    public Account(String name) {
+        this.name = name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
